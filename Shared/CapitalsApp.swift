458081f833ce89050ca18b7dc533772ebefeaef1
MojitoBar <wnehdtjr5@naver.com>
2021-01-29 15:22:47 +0900

    Initial Commit

---
+++ Shared/CapitalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CapitalsApp.swift
+//  Shared
+//
+//  Created by judongseok on 2021/01/29.
+//
+
+import SwiftUI
+
+@main
+struct CapitalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
